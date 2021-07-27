4ef98e39838f67db367681fd477883c72da20f96
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-27 14:47:53 -0300

    Init QRView

--- src/app/components/qrview/qrview.component.ts
+++ src/app/components/qrview/qrview.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { QRDataService } from "../../services/qrdata.service";
+import QRCodeStyling from 'qr-code-styling';
 
 @Component({
   selector: 'app-qrview',
   templateUrl: './qrview.component.html',
   styleUrls: ['./qrview.component.css']
 })
-export class QrviewComponent implements OnInit {
 
-  constructor() { }
+export class QrviewComponent implements OnInit {
+  qrCode:QRCodeStyling;
+  @ViewChild('canvas', { static: true }) canvas: ElementRef;
+
+  constructor(private dataservice: QRDataService) {
+    this.qrCode = new QRCodeStyling(this.dataservice.getQRData());
+  }
 
   ngOnInit(): void {
+    console.log(QRCodeStyling);
+    if (!QRCodeStyling) {
+      return;
+    }
+    this.qrCode.append(this.canvas.nativeElement);
+  }
+
+  update():void{
+    this.qrCode = new QRCodeStyling(this.dataservice.getQRData());
+    this.qrCode.append(this.canvas.nativeElement);
   }
 
 }
