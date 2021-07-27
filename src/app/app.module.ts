77e3a4efbad41d5727c7b8ce5fb945babf567754
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-27 12:37:31 -0300

    Add Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './partials/topbar/topbar.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
