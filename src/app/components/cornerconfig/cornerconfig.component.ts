c67b38b09230e9cc69419415366f29f092f618b3
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-28 15:10:33 -0300

    Add Esquina

--- src/app/components/cornerconfig/cornerconfig.component.ts
+++ src/app/components/cornerconfig/cornerconfig.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { QRDataService } from "../../services/qrdata.service";
 
 @Component({
   selector: 'app-cornerconfig',
   templateUrl: './cornerconfig.component.html',
   styleUrls: ['./cornerconfig.component.css']
 })
-export class CornerconfigComponent implements OnInit {
 
-  constructor() { }
+export class CornerconfigComponent implements OnInit {
+  isDegrade:boolean = false;
+  typeDegrade:string = "lineal";
+  colorP:string = "#71C608";
+  colorD:string = "#71C608";
+  rotation:number = 0;
+  dotType:string = "extra-rounded";
+
+  constructor(private dataservice:QRDataService) { }
 
   ngOnInit(): void {
+  }
+
+  save(){
+    if(this.isDegrade){
+      this.dataservice.setCorners(
+        {
+          color: this.colorP,
+          gradient: {
+            type: this.typeDegrade,
+            rotation: this.rotation,
+            colorStops: [{ offset: 0, color: this.colorP }, { offset: 1, color: this.colorD }]
+          },
+          type: this.dotType
+        }
+      );
+    }else{
+      this.dataservice.setCorners(
+        {
+          color: this.colorP,
+          type: this.dotType
+        }
+      );
+
+    }
+  }
+
+  setDot(strDot:string):void{
+    this.dotType = strDot;
+    this.save();
   }
 
 }
