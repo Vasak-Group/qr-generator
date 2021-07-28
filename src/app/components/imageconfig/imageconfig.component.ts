4b28dc140ac0fdb801608c61ad6040c0feefac2b
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2021-07-28 18:11:05 -0300

    Add Social img

--- src/app/components/imageconfig/imageconfig.component.ts
+++ src/app/components/imageconfig/imageconfig.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { QRDataService } from "../../services/qrdata.service";
 
 @Component({
   selector: 'app-imageconfig',
   templateUrl: './imageconfig.component.html',
   styleUrls: ['./imageconfig.component.css']
 })
-export class ImageconfigComponent implements OnInit {
 
-  constructor() { }
+export class ImageconfigComponent implements OnInit {
+  usarPropia:boolean = false;
+  imagen:string = "";
+
+  constructor(private dataservice:QRDataService) { }
 
   ngOnInit(): void {
+  }
+
+  change(strIMG:string):void{
+    this.imagen = strIMG;
+    this.save();
+  }
+
+  save():void{
+    this.dataservice.setImage(this.imagen);
   }
 
 }
