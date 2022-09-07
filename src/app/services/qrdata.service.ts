1a5c56ae8ea345c9f36a524525417bc3b6a803bd
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-09-07 10:55:51 -0300

    Update Data example

--- src/app/services/qrdata.service.ts
+++ src/app/services/qrdata.service.ts
@@ -5,96 +5,96 @@ import { Injectable } from '@angular/core';
 })
 export class QRDataService {
   qrData = {
-      width: 200,
-      height: 200,
-      type: 'svg',
-      data: "https://qrgenerator.lynx.net.ar/",
-      image: "/assets/img/logo.svg",
-      margin: 10,
-      qrOptions: {
-        typeNumber: 0,
-        mode: 'Byte',
-        errorCorrectionLevel: 'Q'
-      },
-      imageOptions: {
-        hideBackgroundDots: true,
-        imageSize: 0.4,
-        margin: 3,
-        crossOrigin: 'anonymous',
-      },
-      dotsOptions: {
-        color: "#71C608",
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 0,
-        //   colorStops: [{ offset: 0, color: '#8688B2' }, { offset: 1, color: '#77779C' }]
-        // },
-        type: 'rounded'
-      },
-      backgroundOptions: {
-        color: 'rgba(255,255,255,0.6)',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 0,
-        //   colorStops: [{ offset: 0, color: '#ededff' }, { offset: 1, color: '#e6e7ff' }]
-        // },
-      },
-      cornersSquareOptions: {
-        color: '#71C608',
-        type: 'extra-rounded',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 180,
-        //   colorStops: [{ offset: 0, color: '#25456e' }, { offset: 1, color: '#4267b2' }]
-        // },
-      },
-      cornersDotOptions: {
-        color: '#71C608',
-        type: 'dot',
-        // gradient: {
-        //   type: 'linear', // 'radial'
-        //   rotation: 180,
-        //   colorStops: [{ offset: 0, color: '#00266e' }, { offset: 1, color: '#4060b3' }]
-        // },
-      }
+    width: 200,
+    height: 200,
+    type: 'svg',
+    data: "https://qrgenerator.vasak.net.ar/",
+    image: "/assets/img/logo.svg",
+    margin: 10,
+    qrOptions: {
+      typeNumber: 0,
+      mode: 'Byte',
+      errorCorrectionLevel: 'Q'
+    },
+    imageOptions: {
+      hideBackgroundDots: true,
+      imageSize: 0.4,
+      margin: 3,
+      crossOrigin: 'anonymous',
+    },
+    dotsOptions: {
+      color: "#71C608",
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 0,
+      //   colorStops: [{ offset: 0, color: '#8688B2' }, { offset: 1, color: '#77779C' }]
+      // },
+      type: 'rounded'
+    },
+    backgroundOptions: {
+      color: 'rgba(255,255,255,0.6)',
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 0,
+      //   colorStops: [{ offset: 0, color: '#ededff' }, { offset: 1, color: '#e6e7ff' }]
+      // },
+    },
+    cornersSquareOptions: {
+      color: '#71C608',
+      type: 'extra-rounded',
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 180,
+      //   colorStops: [{ offset: 0, color: '#25456e' }, { offset: 1, color: '#4267b2' }]
+      // },
+    },
+    cornersDotOptions: {
+      color: '#71C608',
+      type: 'dot',
+      // gradient: {
+      //   type: 'linear', // 'radial'
+      //   rotation: 180,
+      //   colorStops: [{ offset: 0, color: '#00266e' }, { offset: 1, color: '#4060b3' }]
+      // },
     }
+  }
 
   constructor() { }
 
-  getQRData():object{
+  getQRData(): object {
     return this.qrData;
   }
 
-  setMargin(intMagin:number):void{
+  setMargin(intMagin: number): void {
     this.qrData.margin = intMagin;
   }
 
-  setData(strData:string):void{
+  setData(strData: string): void {
     this.qrData.data = strData;
   }
 
-  setImage(strImage:string):void{
+  setImage(strImage: string): void {
     this.qrData.image = strImage;
   }
 
-  setImageOptions(objImage:any):void{
+  setImageOptions(objImage: any): void {
     this.qrData.imageOptions = objImage;
   }
 
-  setSize(intH:number, intW:number):void{
+  setSize(intH: number, intW: number): void {
     this.qrData.height = intH;
     this.qrData.width = intW;
   }
 
-  setDots(objDots:any):void{
+  setDots(objDots: any): void {
     this.qrData.dotsOptions = objDots;
   }
 
-  setCorners(objCorn:any):void{
+  setCorners(objCorn: any): void {
     this.qrData.cornersSquareOptions = objCorn;
   }
 
-  setCornersDot(objCorn:any):void{
+  setCornersDot(objCorn: any): void {
     this.qrData.cornersDotOptions = objCorn;
   }
 
