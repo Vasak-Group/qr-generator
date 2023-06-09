1f8827744e123cf8f46256eb3e2ce7b99c05c85d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2023-06-09 15:17:50 -0300

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,13 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-echo 'qrgenerator.vasak.net.ar' > CNAME
+echo 'qrgenerator.vasak.net.ar' >CNAME
 
 git init
+git submodule deinit -f --all
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Vasak-Group/qr-generator.git master:gh-pages
+git push -f git@github.com:Vasak-Group/qr-generator.git main:gh-pages
 cd -
