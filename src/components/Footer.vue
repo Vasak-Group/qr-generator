ecb4cddd384b5c8e65ba2d9cc831ae63f48a9fc4
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2025-11-26 15:15:19 -0300

    feat: Add global styling with Tailwind CSS, and introduce Header and Footer components

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,10 +2,10 @@
 </script>
 
 <template>
-  <footer class="bg-white dark:bg-slate-900 py-8 m-4 rounded-xl">
+  <footer class="bg-slate-200 dark:bg-slate-900 py-8 m-4 rounded-xl">
     <div class="container mx-auto px-4 text-center">
-      <p class="text-sm text-slate-500 dark:text-slate-400">
-        &copy; {{ new Date().getFullYear() }} <a  href="https://vasak.net.ar" class="text-primary" target="_blank" rel="noopener noreferrer">Vasak Group</a>. All rights reserved.
+      <p class="text-sm">
+        &copy; {{ new Date().getFullYear() }} <a  href="https://vasak.net.ar" class="text-primary hover:text-secondary transition-colors" target="_blank" rel="noopener noreferrer">Vasak Group</a>. All rights reserved.
       </p>
     </div>
   </footer>
