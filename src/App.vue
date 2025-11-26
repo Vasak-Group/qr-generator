a2227d4719ecaaf1e7a79ae9d9c7dea7adc5ba5d
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2025-11-26 15:17:07 -0300

    feat: add initial application layout and home view with QR generator

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,11 @@ import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col bg-slate-50 dark:bg-slate-950 text-slate-900 dark:text-slate-100 font-sans">
-    <Header />
-    <main class="flex-1">
-      <RouterView />
-    </main>
-    <Footer />
-  </div>
+  <Header />
+  <main>
+    <RouterView />
+  </main>
+  <Footer />
 </template>
 
 <style>
